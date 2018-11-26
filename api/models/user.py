c0c585cc8cb7e081a6606195dc7b89f07aad591e
Matthew <tbernar7@gmail.com>
2018-11-26 14:01:46 -0800

    testing client methods

--- api/models/user.py
+++ api/models/user.py
@@ -34,13 +34,10 @@ class User():
 		self.oauth_key = ''
 
 	def do_request(self, req_func, path, body={}, **params):
-		# get(path, params)
-		# post(path, payload, params)
-		# patch(path, payload)
-		# delete(path)
-
+		'''
+		'''
 		req_dict = {
-			"get": partial(req_func,path, **params),
+			"get": partial(req_func, path, **params),
 			"post": partial(req_func, path, body, **params),
 			"patch": partial(req_func, path, body),
 			"delete": partial(req_func, path)
@@ -100,17 +97,6 @@ class User():
 			response (json): API response to patch
 		'''
 		path = self.paths['users'] + '/' + self.id
-
-		# try:
-		# 	response = self.http.patch(path, body)
-
-		# except (requests.exceptions.HTTPError, api_errors.IncorrectUserCredentials) as e:
-		# 	self.oauth()
-		# 	response = self.http.patch(path, body)
-
-		# except Exception:
-		# 	raise
-
 		response = self.do_request(self.http.patch, path, body=body)
 		self.body = response
 		return response
@@ -127,9 +113,6 @@ class User():
 			self.oauth()
 			response = self.http.post(path, body, **params)
 
-		except Exception:
-			raise
-
 		return Node(response, self.http)
 
 	def get_node(self, node_id, **params):
@@ -140,57 +123,12 @@ class User():
 		full_dehdyrate = 'yes' if params.get('full_dehdyrate') else 'no'
 		force_ref = 'yes' if params.get('force_refresh') else 'no'
 
-		try:
-			response = self.http.get(path, full_dehdyrate=full_dehdyrate, force_refresh=force_ref)
-		
-		except (requests.exceptions.HTTPError, api_errors.IncorrectUserCredentials) as e:
-			self.oauth()
-			response = self.http.get(path, full_dehdyrate=full_dehdyrate, force_refresh=force_ref)
-
-		except Exception:
-			raise
-
+		response = self.do_request(self.http.get, path, full_dehydrate=full_dehydrate, force_refresh=force_refresh)
 		return Node(response, self.http, full_dehdyrate=full_dehdyrate)
 
 	def get_all_nodes(self, **params):
 		'''
 		'''
 		path = self.paths['users'] + '/' + self.id + self.paths['nodes']
-
-		try:
-			response = self.http.get(path, **params)
-		
-		except (requests.exceptions.HTTPError, api_errors.IncorrectUserCredentials) as e:
-			self.oauth()
-			response = self.http.get(path, **params)
-
-		except Exception:
-			raise
-
+		response = self.do_request(self.http.get, path, **params)
 		return Nodes(response, self.http)
-
-	# def get_trans(self, node_id, trans_id):
-	# 	'''gets a specific transaction record
-	# 	Args:
-	# 		user_id (str): user id
-	# 	'''
-	# 	url = self.get_base()
-	# 		+ self.paths['getuser'] + user_id
-	# 		+ self.paths['getnode'] + node_id
-	# 		+ self.paths['gettrans'] + trans_id
-
-	# 	response = self.http.get(url)
-	# 	return Trans(response)
-
-	# def all_user_trans(self, **params):
-	# 	url = self.get_base()
-	# 		+ self.paths['getuser'] + self.id
-	# 		+ self.paths['trans']
-
-	# 	if not self.oauth:
-	# 		self.oauth(self)
-
-	# 	self.http.update_headers(oauth_key=self.oauth)
-
-	# 	response = self.http.get(url)
-	# 	return [Trans(trans_r) for trans_r in response]
