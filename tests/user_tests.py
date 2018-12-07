baacd965a16c3453b439c30ba63accd0767e650a
Matthew <tbernar7@gmail.com>
2018-12-07 12:32:39 -0800

    more user mock tests

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -6,7 +6,7 @@ from .fixtures.client_fixtures import *
 from models.user import User
 from models.http_client import HttpClient
 
-@mock.patch('models.user.User.do_request')
+@mock.patch('models.user.User.do_request', return_value={})
 class UserTests(TestCase):
 
 	def setUp(self):
@@ -17,70 +17,70 @@ class UserTests(TestCase):
 		mock_post.return_value = { 'oauth_key' : '1234'}
 
 	def test_update_info(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_create_node(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_get_node(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_update_node(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_ach_mfa(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_verify_micro(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_reinit_micro(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_ship_debit(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_reset_debit(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_generate_apple_pay(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_dummy_tran(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_delete_node(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_create_trans(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_get_trans(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_comment_trans(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_cancel_trans(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_create_subnet(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_get_subnet(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_get_all_nodes(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_get_all_node_trans(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_get_all_trans(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 	def test_get_all_subnets(self, mock_request):
-		mock_request.return_value = {}
+		pass
 
 if __name__ == '__main__':
 	unittest.main()
