d7ab92c5d78046d4604d3fb22af2b75de6d51def
Matthew Bernardo <tbernar7@gmail.com>
2019-01-17 12:11:11 -0800

    upgrading to Beta 1.0.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.17', # new version number goes here
+    version='1.0.0', # new version number goes here
 
     description='SynapseFi Python Library',
 
@@ -41,7 +41,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
